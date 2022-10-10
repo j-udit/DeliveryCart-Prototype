15badad7232203a98f8b729bd89a97543aebfd02
Judit Medrano <judit.am17@gmail.com>
2022-10-09 21:57:50 -0500

    final

---
+++ DeliveryCart/DeliveryCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delivery.Carts
+{
+    public class DeliveryCart
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
